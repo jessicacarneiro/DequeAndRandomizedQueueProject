7141635559557b716781e4bd861175a3699a205b
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2020-03-02 10:47:30 -0300

    Initial implementation of a randomized queue.

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,45 +1,89 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class RandomizedQueue {
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    private ArrayList<Item> queue;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-
+        this.queue = new ArrayList<>();
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return true;
+        return this.size() == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return this.queue.size();
     }
 
     // add the item
     public void enqueue(Item item) {
-
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        this.queue.add(item);
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        
+        int index = StdRandom.uniform(this.size());
+        
+        return this.queue.remove(index);
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        return null;
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        
+        int index = StdRandom.uniform(this.size());
+        
+        return this.queue.get(index);
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        StdRandom.shuffle(queue.toArray());
+        
+        return new Iterator<Item>() {
+
+            private Iterator<Item> i = queue.iterator();
+
+            @Override
+            public Item next() {
+                if (!i.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return i.next();
+            }
+
+            @Override
+            public void remove() {
+                if (!i.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                
+                i.next();
+                i.remove();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return i.hasNext();
+            }
+        };
     }
-
-    // unit testing (required)
-    public static void main(String[] args) {
-
-    }
-
 }
