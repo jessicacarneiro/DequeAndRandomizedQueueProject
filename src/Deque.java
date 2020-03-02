0e6da90e51be730c2e384f02c53f6cc5200791f7
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2020-03-02 07:40:00 -0300

    Initial deque implementation.

--- src/Deque.java
+++ src/Deque.java
@@ -1,49 +1,95 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque {
-    
+
+    private ArrayList<Item> queue;
+
     // construct an empty deque
     public Deque() {
-
+        this.queue = new ArrayList<Item>();
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return true;
+        return this.queue.size() == 0;
     }
 
     // return the number of items on the deque
     public int size() {
-        return 0;
+        return this.queue.size();
     }
 
     // add the item to the front
     public void addFirst(Item item) {
-        
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.queue.add(0, item);
     }
 
     // add the item to the back
     public void addLast(Item item) {
-        
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.queue.add(item);
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-        return null;
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        return this.queue.remove(0);
     }
 
     // remove and return the item from the back
     public Item removeLast() {
-        return null;
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        int last = this.queue.size() - 1;
+        return this.queue.remove(last);
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return null;
+        return new Iterator<Item>() {
+
+            private Iterator<Item> i = queue.iterator();
+
+            @Override
+            public Item next() {
+                if (!i.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return i.next();
+            }
+
+            @Override
+            public void remove() {
+                if (!i.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                
+                i.next();
+                i.remove();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return i.hasNext();
+            }
+        };
     }
 
-    // unit testing (required)
     public static void main(String[] args) {
-
     }
+    
 }
