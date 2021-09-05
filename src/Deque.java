cd234384afae1542503c1e109218ff6884754e99
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2021-09-05 17:43:04 -0300

    Remove comments from Deque class

--- src/Deque.java
+++ src/Deque.java
@@ -6,22 +6,18 @@ public class Deque<Item> implements Iterable<Item> {
 
     private ArrayList<Item> queue;
 
-    // construct an empty deque
     public Deque() {
         this.queue = new ArrayList<Item>();
     }
 
-    // is the deque empty?
     public boolean isEmpty() {
         return this.queue.size() == 0;
     }
 
-    // return the number of items on the deque
     public int size() {
         return this.queue.size();
     }
 
-    // add the item to the front
     public void addFirst(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
@@ -30,7 +26,6 @@ public class Deque<Item> implements Iterable<Item> {
         this.queue.add(0, item);
     }
 
-    // add the item to the back
     public void addLast(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
@@ -39,7 +34,6 @@ public class Deque<Item> implements Iterable<Item> {
         this.queue.add(item);
     }
 
-    // remove and return the item from the front
     public Item removeFirst() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
@@ -48,7 +42,6 @@ public class Deque<Item> implements Iterable<Item> {
         return this.queue.remove(0);
     }
 
-    // remove and return the item from the back
     public Item removeLast() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
@@ -58,7 +51,6 @@ public class Deque<Item> implements Iterable<Item> {
         return this.queue.remove(last);
     }
 
-    // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
         return new Iterator<Item>() {
 
