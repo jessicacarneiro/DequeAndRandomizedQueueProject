f952ee41e02e948ec4167788f95b8ff346d7a3b8
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2021-09-05 18:13:05 -0300

    Create initial main method

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,6 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
-        
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+
+        while (!StdIn.isEmpty()) {
+            String nextWord = StdIn.readString();
+            randomizedQueue.enqueue(nextWord);
+        }
+
+        for (int i = 0; i < k; i++) {
+            StdOut.println(randomizedQueue.sample());
+        }
     }
 }
