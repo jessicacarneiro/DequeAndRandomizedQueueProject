e7bacdb6409c07fea7371116c01382c129f8620c
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2021-09-05 17:51:15 -0300

    Remove comments from RandomizedQueue class

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -5,59 +5,52 @@ import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    
+
     private ArrayList<Item> queue;
 
-    // construct an empty randomized queue
     public RandomizedQueue() {
         this.queue = new ArrayList<>();
     }
 
-    // is the randomized queue empty?
     public boolean isEmpty() {
         return this.size() == 0;
     }
 
-    // return the number of items on the randomized queue
     public int size() {
         return this.queue.size();
     }
 
-    // add the item
     public void enqueue(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
         }
-        
+
         this.queue.add(item);
     }
 
-    // remove and return a random item
     public Item dequeue() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
-        
+
         int index = StdRandom.uniform(this.size());
-        
+
         return this.queue.remove(index);
     }
 
-    // return a random item (but do not remove it)
     public Item sample() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
-        
+
         int index = StdRandom.uniform(this.size());
-        
+
         return this.queue.get(index);
     }
 
-    // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
         StdRandom.shuffle(queue.toArray());
-        
+
         return new Iterator<Item>() {
 
             private Iterator<Item> i = queue.iterator();
@@ -75,7 +68,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
                 if (!i.hasNext()) {
                     throw new NoSuchElementException();
                 }
-                
+
                 i.next();
                 i.remove();
             }
