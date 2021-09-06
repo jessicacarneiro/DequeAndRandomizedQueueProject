20ea840c57b422757343b88b70930e030b453348
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2021-09-06 16:19:27 -0300

    Revert "Change implementation to use linked-list approach"
    This reverts commit f89c1b743fbc25fe7941e12c02c9ac72e9a9b99e.

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,124 +1,66 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import edu.princeton.cs.algs4.StdRandom;
 
-public class RandomizedQueue<Item> implements Iterable<Item> {
+public class RandomizedQueue<T> implements Iterable<T> {
 
-    private Node firstNode = null;
-    private Node lastNode = null;
-    private int[] indexArray;
-    private int queueSize;
+    private ArrayList<T> queue;
 
     public RandomizedQueue() {
-        this.queueSize = 0;
+        this.queue = new ArrayList<>();
     }
 
     public boolean isEmpty() {
-        return this.queueSize == 0;
+        return this.size() == 0;
     }
 
     public int size() {
-        return this.queueSize;
+        return this.queue.size();
     }
 
-    public void enqueue(Item item) {
+    public void enqueue(T item) {
         if (item == null) {
             throw new IllegalArgumentException();
         }
 
-        if (this.isEmpty()) {
-            firstNode = new Node(item);
-            lastNode = firstNode;
-
-            this.indexArray = new int[1];
-            this.indexArray[0] = 0;
-        } else {
-            Node newNode = new Node(item);
-            lastNode = newNode;
-        }
-
-        resizeIndexArray(this.size() + 1);
-
-        this.queueSize += 1;
+        this.queue.add(item);
     }
 
-    public Item dequeue() {
-        if (this.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-
-        int index = StdRandom.uniform(this.size());
-        Node nodeToRemove = this.firstNode;
-
-        // if single node, lastNode should point to the same place as firstNode (null)
-        if (lastNode == nodeToRemove) {
-            firstNode = firstNode.getNext();
-            lastNode = firstNode.getNext();
-        } else {
-            for (int i = 0; i < index - 1; i++) {
-                nodeToRemove = nodeToRemove.getNext();
-            }
-            nodeToRemove.next = nodeToRemove.getNext().getNext();
-            nodeToRemove = nodeToRemove.getNext();
-        }
-
-        resizeIndexArray(this.size() - 1);
-
-        return (Item) nodeToRemove.getItem();
-    }
-
-    public Item sample() {
+    public T dequeue() {
         if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
 
         int index = StdRandom.uniform(this.size());
 
-        Node sampledNode = this.firstNode;
-
-        for (int i = 0; i < index; i++) {
-            sampledNode = sampledNode.getNext();
-        }
-
-        return (Item) sampledNode.getItem();
+        return this.queue.remove(index);
     }
 
-    public Iterator<Item> iterator() {
-        int[] tempIndexArray = null;
-
-        if (indexArray != null) {
-            tempIndexArray = Arrays.copyOf(indexArray, indexArray.length);
-            StdRandom.shuffle(tempIndexArray);
+    public T sample() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
         }
 
-        int[] finalTempIndexArray = tempIndexArray;
+        int index = StdRandom.uniform(this.size());
 
-        return new Iterator<Item>() {
+        return this.queue.get(index);
+    }
 
-            private int cursor = 0;
-            private int cursorPosition = 0;
+    public Iterator<T> iterator() {
+        StdRandom.shuffle(queue.toArray());
+
+        return new Iterator<T>() {
+
+            private Iterator<T> i = queue.iterator();
 
             @Override
-            public Item next() {
-                if (finalTempIndexArray == null || cursorPosition == indexArray.length) {
+            public T next() {
+                if (!i.hasNext()) {
                     throw new NoSuchElementException();
                 }
-
-                Node node = firstNode;
-
-                for (int i = 0; i < cursor; i++) {
-                    node = node.getNext();
-                }
-
-                if (cursorPosition < indexArray.length - 1) {
-                    cursor = finalTempIndexArray[cursorPosition + 1];
-                }
-
-                cursorPosition += 1;
-
-                return (Item) node.getItem();
+                return i.next();
             }
 
             @Override
@@ -128,49 +70,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
             @Override
             public boolean hasNext() {
-                return finalTempIndexArray != null && cursorPosition < indexArray.length;
+                return i.hasNext();
             }
         };
-    }
-
-    private class Node<Item> {
-        Item item;
-        Node next;
-
-        public Node(Item item) {
-            this.item = item;
-        }
-
-        public Node(Item item, Node next) {
-            this.item = item;
-            this.next = next;
-        }
-
-        public Item getItem() {
-            return item;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-    }
-
-    private void resizeIndexArray(int newSize) {
-        int[] newIndexArray = new int[newSize];
-
-        if (newSize > this.size()) {
-            for (int i = 0; i < this.size(); i++) {
-                newIndexArray[i] = this.indexArray[i];
-            }
-            newIndexArray[this.size()] = this.size();
-        }
-        else {
-            for (int i = 1; i < this.size(); i++) {
-                newIndexArray[i-1] = this.indexArray[i];
-            }
-        }
-
-        this.indexArray = newIndexArray;
     }
 
     public static void main(String[] args) {
